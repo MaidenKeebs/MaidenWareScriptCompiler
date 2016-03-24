10874cc0bd03d1102b51689686ea9832c1bdae78
Ashton Keeble <keebs.maidenware@gmail.com>
2016-03-24 17:07:14 +0000

    Project Set-Up I've just simply set up the Visual Studio project.

---
+++ MWSCompiler/MWSCompiler/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MWSCompiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MaidenWare-Script Compiler v(0.0.0.1)");
+            Console.ReadLine();
+        }
+    }
+}
